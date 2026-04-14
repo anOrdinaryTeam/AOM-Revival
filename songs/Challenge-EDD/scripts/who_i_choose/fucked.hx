8def9f906318101d43342be4f64d4c979549c733
!! Fredd. <95300490+wohfredd@users.noreply.github.com>
2026-04-14 15:42:36 -0500

    andamo chistoso

--- songs/Challenge-EDD/scripts/who_i_choose/fucked.hx
+++ songs/Challenge-EDD/scripts/who_i_choose/fucked.hx
@@ -1,5 +1,5 @@
 function create() {
     for (f in ['bf-tord', 'edd-tord', 'matt-tord', 'tomRunsIn', 'tordBot', 'tordGlass', 'tordBG', 'tordHelicopter', 'tordFlails', 'bf-lookup'])
-        { graphicCache.cache(getModImage('Challenge-EDD/fucked/' + f)); }
+        { graphicCache.cache(getModImage('Challenge-EDD/fucked/$f')); }
     precacheCharacter(0, 'tord');
 }
