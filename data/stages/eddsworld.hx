48509f4915003bfb1572f1e049b8ba2a34289d32
!! Fredd. <95300490+wohfredd@users.noreply.github.com>
2026-04-14 13:39:56 -0500

    fix

--- data/stages/eddsworld.hx
+++ data/stages/eddsworld.hx
@@ -3,6 +3,8 @@ var plane:FlxSprite;
 function create() {
     defaultCamZoom = 0.65;
 
+    graphicCache.cache(getModImage('Challenge-EDD/plane'));
+
     boyfriend.setPosition(1160, 90);
     gf.setPosition(605, 94);
     dad.setPosition(286, 190);
@@ -15,37 +17,40 @@ function create() {
     clouds.scrollFactor.set(0.15, 0.15);
     insert(2, clouds);
 
-    plane = new FlxSprite(1100, 0, getModImage('Challenge-EDD/plane'));
-    plane.scrollFactor.set(0.2, 0.2);
-    plane.scale.set(1.5, 1.5);
-    insert(3, plane);
-
     var parallax:FlxSprite = new FlxSprite(-710, 300, getModImage('Challenge-EDD/secParallax'));
     parallax.scrollFactor.set(0.75, 0.75);
     parallax.scale.set(1.5, 1.5);
-    insert(4, parallax);
+    insert(3, parallax);
 
     var house:FlxSprite = new FlxSprite(-950, -50, getModImage('Challenge-EDD/house'));
     house.scale.set(1.5, 1.5);
-    insert(5, house);
+    insert(4, house);
 
     var fence:FlxSprite = new FlxSprite(-390, 430, getModImage('Challenge-EDD/fence'));
     fence.scale.set(1.5, 1.5);
-    insert(6, fence);
+    insert(5, fence);
 
     var car:FlxSprite = new FlxSprite(-1175, 750, getModImage('Challenge-EDD/car'));
-    insert(7, car);
+    insert(5, car);
+}
+
+function stepHit() {
+    switch(curStep) {
+        case 380: Plane();
+    }
 }
 
 function postCreate() {
     loadHud('VS-Online');
 }
 
-function stepHit() {
-    switch(curStep) {
-        case 380:
-            FlxTween.tween(plane, {x: 1900}, 10, {onComplete: function() {
-                remove(plane);
-            }});
-    }
+function Plane() {
+    plane = new FlxSprite(1200, 0, getModImage('Challenge-EDD/plane'));
+    plane.scrollFactor.set(0.2, 0.2);
+    plane.scale.set(1.5, 1.5);
+    insert(3, plane);
+
+    FlxTween.tween(plane, {x: 1900}, 10, {onComplete: function() {
+        remove(plane);
+    }});
 }
