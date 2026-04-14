63ccd280db968c83c12cf1f6989d00e24baf9e8c
Lite6G <96150137+Lite6G@users.noreply.github.com>
2026-04-14 18:30:59 -0400

    why
    por que tengo que ponerle nombre a todas las pull si solo quiero subir como 3 mierditas

---
+++ data/stages/rush.hx
@@ -0,0 +1,14 @@
+function postCreate()
+    loadHud('PsychEngine', 'Fred, mamalo.');
+
+function create() {
+
+    defaultCamZoom = 0.5;
+
+    useCamMov = true;
+    camMoveAmt = 50;
+
+    boyfriend.setPosition(870, 100);
+    dad.setPosition(100, 100);
+
+}
