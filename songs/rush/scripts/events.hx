517937aa41f85b8d54a6b6ae908df0a237a42fb0
Lite6G <96150137+Lite6G@users.noreply.github.com>
2026-04-15 12:55:10 -0400

    Update events.hx

--- songs/rush/scripts/events.hx
+++ songs/rush/scripts/events.hx
@@ -1,16 +1,19 @@
+var upline:FlxSprite;
+var downline:FlxSprite;
+
 function create() {
 
-    var upline:FlxSprite = new FlxSprite().makeSolid(FlxG.width*2 , 60, 0xFF000000);
+    upline = new FlxSprite().makeSolid(FlxG.width*2 , 60, 0xFF000000);
     //upline.setPosition(-1400,-750);
     upline.camera = camHUD;
     insert(1, upline);
 
-    var downline:FlxSprite = new FlxSprite().makeSolid(FlxG.width*2, 70, 0xFF000000);
+    downline = new FlxSprite().makeSolid(FlxG.width*2, 70, 0xFF000000);
     //downline.setPosition(-1400,832.5);
     downline.setPosition(0,658);
     downline.camera = camHUD;
     insert(1, downline);
-
+    
 }
 
 function stepHit()
@@ -22,5 +25,9 @@ function stepHit()
 
         case 517, 581, 768:
             dad.playAnim("Blaze yeah", true);
+
+        case 5:
+            var FlxTween.tween(upline, {y: upline.y - 100}, 1, {ease: FlxEase.backInOut});
+            var FlxTween.tween(strum, {y: strum.y - 600, angle: -360});
 
     }
