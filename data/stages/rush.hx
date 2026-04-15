6aa2fcd9fb76f835e39912d10cf9f5d58bdb59ec
Lite6G <96150137+Lite6G@users.noreply.github.com>
2026-04-14 21:52:45 -0400

    Movi lo demas a Events

--- data/stages/rush.hx
+++ data/stages/rush.hx
@@ -29,15 +29,3 @@ function create() {
     // ruhs - Fred
 
 }
-
-function stepHit()
-
-    switch(curStep){
-
-        case 513, 577, 768:
-            boyfriend.playAnim("Sonic Yeah", true);
-
-        case 517, 581, 768:
-            dad.playAnim("Blaze yeah", true);
-
-    }
