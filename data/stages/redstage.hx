1c2ddfe4969084ca5f69b5be2d788f8c18d64d20
Lite6G <96150137+Lite6G@users.noreply.github.com>
2026-04-15 19:05:29 -0400

    eh

--- data/stages/redstage.hx
+++ data/stages/redstage.hx
@@ -1,2 +1,19 @@
-function postCreate()
-    loadHud('KadeEngine', '1.5.4 (Hijos de la verga)');
+function postCreate(){
+
+    loadHud('KadeEngine', '1.5.4');
+    for (strum in cpu)
+        strum.visible = false; 
+    for (i in playerStrums) i.x -= 640;
+    for (hb in [healthBar, healthBarBG])
+        hb.flipX = true;
+
+}
+
+function create(){
+        
+
+    var fondo:FlxSprite = new FlxSprite(-852,-475, getModImage('pokemonmaster/fondo_pokemon_1'));
+    defaultCamZoom = 0.85;
+    insert(1,fondo);
+
+}
