4fb8e73c6424fee7619115cf64ee7111f227b608
Lite6G <96150137+Lite6G@users.noreply.github.com>
2026-04-14 22:35:52 -0400

    ruhs - Fred

--- data/stages/rush.hx
+++ data/stages/rush.hx
@@ -14,17 +14,6 @@ function create() {
     var bg:FlxSprite = new FlxSprite().makeSolid(FlxG.width * 3, FlxG.height * 3, 0xFFFFFFFF);
     bg.setPosition(-1400,-700);
     insert(0, bg);
-
-    var upline:FlxSprite = new FlxSprite().makeSolid(FlxG.width*2 , 60, 0xFF000000);
-    //upline.setPosition(-1400,-750);
-    upline.camera = camHUD;
-    insert(1, upline);
-
-    var downline:FlxSprite = new FlxSprite().makeSolid(FlxG.width*2, 70, 0xFF000000);
-    //downline.setPosition(-1400,832.5);
-    downline.setPosition(0,658);
-    downline.camera = camHUD;
-    insert(1, downline);
     
     // ruhs - Fred
 
--- songs/rush/scripts/events.hx
+++ songs/rush/scripts/events.hx
@@ -9,3 +9,18 @@ function stepHit()
             dad.playAnim("Blaze yeah", true);
 
     }
+
+function create() {
+
+    var upline:FlxSprite = new FlxSprite().makeSolid(FlxG.width*2 , 60, 0xFF000000);
+    //upline.setPosition(-1400,-750);
+    upline.camera = camHUD;
+    insert(1, upline);
+
+    var downline:FlxSprite = new FlxSprite().makeSolid(FlxG.width*2, 70, 0xFF000000);
+    //downline.setPosition(-1400,832.5);
+    downline.setPosition(0,658);
+    downline.camera = camHUD;
+    insert(1, downline);
+
+}
