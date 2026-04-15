888a8ce2f918f50cbea78a0b8d874478d0d1d1ee
Lite6G <96150137+Lite6G@users.noreply.github.com>
2026-04-14 23:22:12 -0400

    gyaru.

---
+++ data/menuChars/abo/script.hx
@@ -0,0 +1,13 @@
+function create()
+        FlxG.sound.play(Paths.sound('menusounds/gyaru1'), volume = 4);
+
+function postUpdate() {
+
+    var abo:Character = charactersMenu.members[0];
+
+    if (CoolUtil.mouseOverlaps(abo) && FlxG.mouse.justPressed) {
+
+        FlxG.sound.play(Paths.sound('menusounds/gyaru2'), volume = 4);
+
+    }
+}
