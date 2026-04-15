d07fd1a7606c1527bbd0c6316a3976df58dac986
Lite6G <96150137+Lite6G@users.noreply.github.com>
2026-04-14 21:33:12 -0400

    67

--- data/stages/rush.hx
+++ data/stages/rush.hx
@@ -34,10 +34,10 @@ function stepHit()
 
     switch(curStep){
 
-        case 513, 577:
+        case 513, 577, 768:
             boyfriend.playAnim("Sonic Yeah", true);
 
-        case 517, 581:
+        case 517, 581, 768:
             dad.playAnim("Blaze yeah", true);
 
     }
