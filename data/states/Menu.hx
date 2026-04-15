40172173bd5667b55a2361d80e920aba41757982
!! Fredd. <95300490+wohfredd@users.noreply.github.com>
2026-04-14 22:07:50 -0500

    ostia

--- data/states/Menu.hx
+++ data/states/Menu.hx
@@ -15,7 +15,7 @@ public var danceOnBeat:Bool = true;
 public var charactersMenu:FlxTypedGroup<Character> = new FlxTypedGroup();
 
 // TESTING
-var charMenu:String = 'rushsonic';
+var charMenu:String = '';
 
 function create() {
     FlxG.mouse.visible = true;
