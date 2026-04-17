3a1e497f11abd814bc2603a8adf82857f93ea2eb
Solex <95106797+SantiYea@users.noreply.github.com>
2026-04-17 20:46:07 -0300

    Accelerant Fucked Chart + Less earrape Bullet Sound

--- data/notes/Bullet Note.hx
+++ data/notes/Bullet Note.hx
@@ -1,6 +1,13 @@
 var t:String = 'Bullet Note';
 var cancelHankAnim:Bool = false;
+
 var timer:FlxTimer = new FlxTimer();
+var bulletSound:FlxSound;
+
+function postCreate() {
+    bulletSound = FlxG.sound.load(Paths.getPath('Assets-RandomSongs/sounds/bullet.ogg'));
+    bulletSound.volume = 0.45;
+}
 
 function shootTricky() if (shootAtTricky) {
     deimos.playAnim('shoot', true);
@@ -21,7 +28,7 @@ function onPlayerHit(e) if (e.noteType == t) {
     timer.start(0.2, () -> cancelHankAnim = false);
 
     shootTricky();
-    if (curDiff == 'fucked') playModSound('bullet', 0.5);
+    if (curDiff == 'fucked') bulletSound.play(true);
     camGame.shake(0.01, 0.1);
 }
 
@@ -33,7 +40,7 @@ function onPlayerMiss(e) if (e.noteType == t) {
     timer.start(0.3, () -> cancelHankAnim = false);
 
     shootTricky();
-    if (curDiff == 'fucked') playModSound('bullet', 0.5);
+    if (curDiff == 'fucked') bulletSound.play(true);
     camGame.shake(0.01, 0.1);
 }
 
---
+++ data/notes/Expurgation Note.hx
@@ -0,0 +1,19 @@
+var t:String = 'Expurgation Note';
+
+function onNoteCreation(_) if (_.noteType == t) {
+    _.noteSprite = 'modNotes/Tricky/deathNotes';
+    _.note.forceIsOnScreen = true;
+    _.note.earlyPressWindow = 0.1;
+	_.note.latePressWindow = 0.2;
+    if (_.strumLineID <= 0) _.note.wasGoodHit = true;
+}
+
+function onPostNoteCreation(_) if (_.noteType == t) {
+    _.note.offset.x -= 1;
+    _.note.offset.y = 55;
+}
+
+function onPlayerMiss(e) if (e.noteType == t) {
+	e.cancel();
+	deleteNote(e.note); 
+}
