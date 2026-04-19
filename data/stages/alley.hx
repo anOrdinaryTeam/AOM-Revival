d787d1a4857793f70af0180bbe3301c9c4f1412b
Solex <95106797+SantiYea@users.noreply.github.com>
2026-04-19 18:32:17 -0300

    Change Note Skin Function
    Totally necessary to use `graphicCache.cache()` for do not have any Spikelag

--- data/stages/alley.hx
+++ data/stages/alley.hx
@@ -47,4 +47,3 @@ function create() {
         addSprite(floor);
     }
 }
-
--- songs/Management.hx
+++ songs/Management.hx
@@ -1,6 +1,7 @@
 import funkin.backend.utils.DiscordUtil;
 import funkin.editors.charter.Charter;
 importScript('data/scripts/ExternalFunctions');
+using StringTools;
 
 public var songName:String = PlayState.SONG.meta.name;
 public var curDiff:String = PlayState.difficulty;
@@ -35,6 +36,37 @@ public function setRatingPrefix(tag:String) {
     ratingPrefix = tag;
     var folder:Array<String> = Paths.getFolderContent('images/modCombos/$ratingPrefix', false, 1, true);
     for (graphic in folder) graphicCache.cache(Paths.image('modCombos/Eteled/$graphic'));
+}
+
+public function changeNoteSkin(path:String, _strum:Dynamic, _part:String = 'both') if (_strum != null) {
+    var part:String = _part.toLowerCase();
+    if (!Assets.exists(Paths.image(path)) || !Assets.exists(Paths.getPath('images/$path.xml'))) {
+        trace('Sprisheet/XML doesnt exists - [$path]');
+        return;
+    }
+
+    if (part == 'strum' || part == 'strums' || part == 'both') for (i => strum in _strum.members) {
+        var prefixes:Array<String> = ['LEFT', 'DOWN', 'UP', 'RIGHT'];
+        strum.frames = Paths.getSparrowAtlas(path);
+        strum.animation.addByPrefix('static', 'arrow${prefixes[i]}');
+        strum.animation.addByPrefix('pressed', '${prefixes[i].toLowerCase()} press', 24, false);
+        strum.animation.addByPrefix('confirm', '${prefixes[i].toLowerCase()} confirm', 24, false);
+        strum.updateHitbox();
+    }
+
+    if (part == 'note' || part == 'notes' || part == 'both') for (babyArrow in _strum.notes) {
+        var lastAnim:String = babyArrow.animation.name;
+        var color:String = ["purple", "blue", "green", "red"][babyArrow.strumID % 4];
+
+        babyArrow.frames = Paths.getFrames(path);
+        babyArrow.animation.addByPrefix(lastAnim, switch(lastAnim){
+            case 'scroll': '${color}0';
+            case 'hold': '${color} hold piece';
+            case 'holdend': '${(color == "purple" ? 'pruple end hold' : '${color} hold end')}0';
+        });
+        babyArrow.animation.play(lastAnim);
+        babyArrow.updateHitbox();
+    }
 }
 
 function create() {
