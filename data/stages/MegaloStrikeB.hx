e20d703982ffca9e950525eb123037085675ec53
!! Fredd. <95300490+wohfredd@users.noreply.github.com>
2026-04-29 10:30:21 -0500

    msb

--- data/stages/MegaloStrikeB.hx
+++ data/stages/MegaloStrikeB.hx
@@ -1,6 +1,6 @@
 function create() {
     defaultCamZoom = 0.8;
-    var bg:FlxSprite = new FlxSprite(-600, -300, getModImage('chara-bg'));
+    var bg:FlxSprite = new FlxSprite(-600, -300, getModImage('MegaloStrikeB/chara-bg'));
     bg.antialiasing = Options.antialiasing;
     addSprite(bg);
 }
