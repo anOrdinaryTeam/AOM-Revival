2a7e8dae74ed6eebfaf71fa97c5427b2f678746d
!! Fredd. <95300490+wohfredd@users.noreply.github.com>
2026-05-03 19:41:17 -0500

    nyaw

--- data/stages/Kapi-stage.hx
+++ data/stages/Kapi-stage.hx
@@ -11,9 +11,8 @@ function onNoteCreation(_) {
     _.note.splash = 'Kapi/v1';
 }
 
-var back:FlxSprite;
-var lights:FunkinSprite;
-var front:FlxSprite;
+var audience:FunkinSprite;
+var freaks:FunkinSprite;
 
 function create() {
     // settings shit
@@ -28,27 +27,61 @@ function create() {
     // settings shit
 
     // stage shit
-    back = new FlxSprite(-715, -230);
-    if (curSong == 'wocky' || 'beathoven') {
-        back.loadGraphic(getModImage('stage/old'));
-    }
-    else if (curSong == 'hairball') {
-        back.getModImage('stage/sunset');
-    }
-    else if (curSong == 'nyaw') {
-        back.getModImage('stage/closed');
-    }
+    var back:FlxSprite = new FlxSprite(-715, -230);
     back.scrollFactor.set(0.9, 0.9);
     insert(0, back);
+    if (curSong == 'wocky' || curSong == 'beathoven') {
+        back.loadGraphic(getModImage('stage/old'));
+    }
+    if (curSong == 'hairball') {
+        back.loadGraphic(getModImage('stage/sunset'));
+    }
+    if (curSong == 'nyaw') {
+        back.loadGraphic(getModImage('stage/closed'));
 
-    lights = new FunkinSprite(-715, -230).loadSprite(getModImage('stage/lights'));
+        freaks = new FunkinSprite(-625, -200).loadSprite(getModImage('stage/bgFreaks'));
+        freaks.scrollFactor.set(0.9, 0.9);
+        freaks.addAnim('idle', 'Bottom Level Boppers', 28, false);
+        insert(1, freaks);
+    }
+    if (curSong == 'beathoven' || curSong == 'hairball') {
+        var spookys:FunkinSprite = new FunkinSprite(-25, 150).loadSprite(getModImage('stage/littleguys'));
+        spookys.scrollFactor.set(0.9, 0.9);
+        spookys.addAnim('idle', 'Bottom Level Boppers', 24, true);
+        spookys.playAnim('idle');
+        insert(1, spookys);
+    }
+    if (curSong == 'hairball' || curSong == 'nyaw') {
+        audience = new FunkinSprite(-625, -200).loadSprite(getModImage('stage/upperBop'));
+        audience.scrollFactor.set(0.9, 0.9);
+        audience.addAnim('idle', 'Upper Crowd Bob', 18, false);
+        add(audience);
+    }
+    
+    var lights:FunkinSprite = new FunkinSprite(-715, -230).loadSprite(getModImage('stage/lights'));
     lights.scrollFactor.set(0.9, 0.9);
     lights.addAnim('idle', 'lightblink', 1, true);
     lights.playAnim('idle');
-    insert(1, lights);
+    insert(3, lights);
 
-    front = new FlxSprite(-650, 600, getModImage('stage/front'));
-    // front.scrollFactor.set(0.9, 0.9);
+    var front:FlxSprite = new FlxSprite(-650, 600, getModImage('stage/front'));
     front.scale.set(1.1, 1.1);
-    insert(2, front);
+    insert(4, front);
+    // stage shit
+}
+
+function beatHit() {
+    if (curBeat % 2 == 0) {
+        if (curSong == 'hairball') {
+            audience.playAnim('idle');
+        }
+        if (curSong == 'nyaw') {
+            audience.playAnim('idle');
+            freaks.playAnim('idle');
+        }
+    }
+    if (curSong == 'nyaw' && curBeat == 437) {
+        camGame.alpha = 0;
+        camHUD.alpha = 0;
+    }
 }
