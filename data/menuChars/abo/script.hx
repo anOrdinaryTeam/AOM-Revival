f80766d39bab51032ad259214e402e8af6d5de99
!! Fredd. <95300490+wohfredd@users.noreply.github.com>
2026-05-23 18:17:20 -0500

    lmao

--- data/menuChars/abo/script.hx
+++ data/menuChars/abo/script.hx
@@ -1,9 +1,9 @@
 function create()
-        FlxG.sound.play(Paths.sound('menusounds/gyaru1'), volume = 4);
+        FlxG.sound.play(Paths.sound('menusounds/gyaru1'));
 
 function postUpdate() {
     var abo:Character = charactersMenu.members[0];
     if (CoolUtil.mouseOverlaps(abo) && FlxG.mouse.justPressed) {
-        FlxG.sound.play(Paths.sound('menusounds/gyaru2'), volume = 4);
+        FlxG.sound.play(Paths.sound('menusounds/gyaru2'));
     }
 }
