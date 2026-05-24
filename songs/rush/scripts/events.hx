e0b81e1425ffd7ce973a72763fc3a70bea6b2024
!! Fredd. <95300490+wohfredd@users.noreply.github.com>
2026-05-23 19:43:00 -0500

    lmao lmaolmao lmaolmao lmaolmao lmaolmao lmaolmao lmaolmao lmaolmao lmaolmao lmaolmao lmaolmao lmaolmao lmaolmao lmaolmao lmaolmao lmaolmao lmaolmao lmaolmao lmaolmao lmaolmao lmaolmao lmaolmao lmaolmao lmaolmao lmaolmao lmaolmao lmao

--- songs/rush/scripts/events.hx
+++ songs/rush/scripts/events.hx
@@ -32,7 +32,7 @@ function stepHit()
             for (strum in cpu)
                 FlxTween.tween(strum, {y: 120}, 1, {ease: FlxEase.backInOut});
 
-            // for (acheude in [healthBar, healthBarBG, iconP1, iconP2])
+            for (acheude in [healthBar, healthBarBG, iconP1, iconP2])
                 FlxTween.tween(acheude, {alpha: 0}, 0.5);
 
         case 577:
