d494733eee075193810295d66e0263c9e1bd01fd
!! Fredd. <95300490+wohfredd@users.noreply.github.com>
2026-06-30 13:24:42 -0500

    less forever hud hardcode

--- data/HUDS/ForeverEngine.hx
+++ data/HUDS/ForeverEngine.hx
@@ -1,14 +1,11 @@
-import funkin.backend.system.macros.GitCommitMacro;
 import flixel.text.FlxTextBorderStyle;
-
 public var hudItems:FlxTypedGroup<Dynamic> = new FlxTypedGroup();
 
 var ratingsInt:Map<String, Int> = ["sick" => 0, "good" => 0, "bad" => 0, "shit" => 0];
+var ratingFix:String;
 
 var missNumScale:Float;
 var missRatingScale:Float;
-
-var isGold:Bool = true;
 
 function onHudLoad(hud) if (hud == 'ForeverEngine') {
     hudItems.camera = camHUD;
@@ -19,21 +16,20 @@ function onHudLoad(hud) if (hud == 'ForeverEngine') {
     score.screenCenter(FlxAxes.X);
     hudItems.add(score);
 
-    var txtMark:String = getSaveData('Forever_Watermark') ? 'FOREVER ENGINE v0.3.1' : 'CODENAME ENGINE ${GitCommitMacro.commitHash}';
-    var cornerMark:FunkinText = new FunkinText(0, 0, 0, txtMark);
+    var cornerMark:FunkinText = new FunkinText(0, 0, 0, getSaveData('Forever_Watermark') ? 'FOREVER ENGINE v0.3.1' : Flags.VERSION_MESSAGE.toUpperCase());
     cornerMark.setFormat(Paths.font('vcr.ttf'), 18, FlxColor.WHITE, 'left', FlxTextBorderStyle.OUTLINE, FlxColor.BLACK, 2);
-	cornerMark.setPosition(FlxG.width - (cornerMark.width + 5), 5);
-    cornerMark.antialiasing = Options.antialiasing;
+    cornerMark.setPosition(FlxG.width - (cornerMark.width + 5), 5);
     hudItems.add(cornerMark);
 
     if (getSaveData('Forever_Info')) {
         var centerMark:FunkinText = new FunkinText(0, 0, 0, '- ${PlayState.SONG.meta?.name} [${curDiff.toUpperCase()}] -');
         centerMark.setFormat(Paths.font('vcr.ttf'), 24, FlxColor.WHITE, 'center', FlxTextBorderStyle.OUTLINE, FlxColor.BLACK, 2);
-        centerMark.y = (FlxG.height / 24) - 24;
-        centerMark.antialiasing = Options.antialiasing;
-        centerMark.screenCenter(FlxAxes.X);
-        centerMark.alpha = getSaveData('Forever_Opacity');
         hudItems.add(centerMark);
+
+        centerMark.y = (FlxG.height / 24) - 24;
+        centerMark.screenCenter(FlxAxes.X);
+
+        centerMark.alpha = getSaveData('Forever_Opacity');
     }
 
     for (i in [healthBar, healthBarBG, iconP1, iconP2, score, cornerMark])
@@ -50,8 +46,6 @@ function onHudLoad(hud) if (hud == 'ForeverEngine') {
 function update() for (icons in [iconP1, iconP2]) {
 	icons.setGraphicSize(Std.int(FlxMath.lerp(150, icons.width, .5)));
 	icons.updateHitbox();
-
-    isGold = (accuracy == -1 || accuracy == 1);
 }
 
 function beatHit() for (icons in [iconP1, iconP2]) {
@@ -60,127 +54,52 @@ function beatHit() for (icons in [iconP1, iconP2]) {
 }
 
 function onRatingUpdate(_) {
-    var quanAcc:Float = CoolUtil.quantize(accuracy * 100, 100);
-    var str = 'Score: $songScore • Accuracy: $quanAcc% ${getAccuracyLetter()} • ${getSaveData('Forever_MissType')}: $misses • Rank: ${getRankLetter(quanAcc, _)}';
-    hudItems.members[0]?.text = str;
+    hudItems.members[0]?.text = 'Score $songScore • Accuracy: ${CoolUtil.quantize(accuracy * 100, 100)}% ';
+
+    if (getSaveData('Forever_Ratings') && misses == 0)
+        hudItems.members[0]?.text += '${getRating()} ';
+
+    hudItems.members[0]?.text += '• ${getSaveData('Forever_MissType')}: $misses • Rank: ${_.rating.rating}';
     hudItems.members[0]?.screenCenter(FlxAxes.X);
 }
 
-function onPlayerHit(_) {    
+function getRating() {
+    if (misses == 0 && ratingsInt['sick'] >= 1 && ratingsInt['good'] == 0 && ratingsInt['bad'] == 0 && ratingsInt['shit'] == 0)
+        return '[SFC]';
+    else if (misses == 0 && ratingsInt['good'] >= 1 && ratingsInt['bad'] == 0 && ratingsInt['shit'] == 0)
+        return '[GFC]';
+    else if (misses == 0)
+        return '[FC]';
+}
+
+function onPlayerHit(_) {
     ratingsInt[_.rating] += 1;
 
-    if (getSaveData('Forever_Combo')) {
-        _.displayRating = _.showRating = false;
-
-        if (!_.note.isSustainNote)
-            createRating(_);
-
-        missNumScale = _.numScale; 
-        missRatingScale = _.ratingScale;
-    }
     if (getSaveData('Forever_Splash'))
         _.note.splash = "Forever/forever";
+
+    if (getSaveData('Forever_Combo') && _.note.strumTime < Conductor.songPosition)
+        ratingFix = '-late';
+    else 
+        ratingFix = '-early';
+}
+
+function onRatingsShown(_) {
+    if (!getSaveData('Forever_Combo')) return;
+
+    if (accuracy == -1 || accuracy == 1)
+        _.ratingPrefix = 'modCombos/ForeverEngine/';
+
+    switch(_.rating) {
+        case 'good' | 'bad' | 'shit':
+            _.ratingPrefix = 'modCombos/ForeverEngine/';
+            _.ratingSuffix = ratingFix;
+    }
 }
 
 function onPlayerMiss()
     if (getSaveData('Forever_Miss'))
         createRatingMiss();
-
-function getAccuracyLetter() {
-    var rating:String = '';
-
-    if (getSaveData('Forever_Ratings')) {
-        if (misses == 0 && ratingsInt['sick'] >= 1 && ratingsInt['good'] == 0 && ratingsInt['bad'] == 0 && ratingsInt['shit'] == 0)
-            rating = '[SFC]';
-        else if (misses == 0 && ratingsInt['good'] >= 1 && ratingsInt['bad'] == 0 && ratingsInt['shit'] == 0)
-            rating = '[GFC]';
-        else if (misses == 0)
-            rating = '[FC]';
-        else 
-            rating = '';
-    }
-
-    return rating;
-}
-
-function getRankLetter(rank, cneRating) {
-    if (getSaveData('Forever_Ratings')) {
-        if (rank >= 100) return 'S+';
-        if (rank >= 95)  return 'S';
-        if (rank >= 90)  return 'A';
-        if (rank >= 85)  return 'B';
-        if (rank >= 80)  return 'C';
-        if (rank >= 75)  return 'D';
-        if (rank >= 70)  return 'E';
-        if (rank >= 65)  return 'F';
-    }
-    else {
-        rank = cneRating.rating.rating;
-    }
-    return rank;
-}
-
-function createRating(shit) {
-    var shitScore = []; 
-	if (combo >= 1000)
-        shitScore.push(Math.floor(combo / 1000) % 10);
-    if (combo >= 100)
-	    shitScore.push(Math.floor(combo / 100) % 10);
-    if (combo >= 10)
-	    shitScore.push(Math.floor(combo / 10) % 10);
-
-    if (combo == 0) combo = 1;
-	shitScore.push(combo % 10);
-
-    var i = 0;
-    for (n in shitScore) {
-        var numScore:FlxSprite = new FlxSprite((43 * i) + comboGroup.x, comboGroup.y + 150);
-        numScore.loadGraphic(Paths.image((isGold ? 'modCombos/ForeverEngine/num$n' : 'game/score/num$n')));
- 
-		numScore.acceleration.y = FlxG.random.int(200, 300);
-		numScore.velocity.y -= FlxG.random.int(140, 160);
-		numScore.velocity.x = FlxG.random.float(-10, 10);
-
-        numScore.scale.set(shit.numScale, shit.numScale);
-		numScore.updateHitbox();
-        
-		numScore.antialiasing = Options.antialiasing;
-        add(numScore);
-
-		FlxTween.tween(numScore, {alpha: 0}, 0.2, {
-            startDelay: Conductor.crochet * 0.002,
-			onComplete: () -> remove(numScore, true)
-		});
-        i++;
-    }
-
-    var rating:FlxSprite = new FlxSprite(comboGroup.x, comboGroup.y);
-    rating.loadGraphic(Paths.image((isGold ? 'modCombos/ForeverEngine/sick' : 'game/score/sick')));
-
-    var ratingSuffix:String = '';
-    if (shit.note.strumTime < Conductor.songPosition)
-        ratingSuffix = 'late';
-    else
-        ratingSuffix = 'early';
-
-    if (shit.rating != 'sick')
-        rating.loadGraphic(Paths.image('modCombos/ForeverEngine/${shit.rating}-$ratingSuffix'));
-
-    rating.acceleration.y = 550;
-    rating.velocity.y -= FlxG.random.int(140, 175);
-    rating.velocity.x -= FlxG.random.int(0, 10);
-
-    rating.scale.set(shit.ratingScale, shit.ratingScale);
-    rating.updateHitbox();
-
-    rating.antialiasing = Options.antialiasing;
-    add(rating);
-
-    FlxTween.tween(rating, {alpha: 0}, 0.2, {
-        startDelay: Conductor.crochet * 0.001,
-        onComplete: () -> remove(rating, true)
-    });
-}
 
 function createRatingMiss() {
     var fixMisses:Int = 1;
@@ -206,7 +125,7 @@ function createRatingMiss() {
 		numScore.velocity.y -= FlxG.random.int(140, 160);
 		numScore.velocity.x = FlxG.random.float(-10, 10);
 
-        numScore.scale.set(missNumScale, missNumScale);
+        numScore.scale.set(.5, .5);
 		numScore.updateHitbox();
         
 		numScore.antialiasing = Options.antialiasing;
@@ -227,7 +146,7 @@ function createRatingMiss() {
     	line.velocity.y -= FlxG.random.int(140, 160);
     	line.velocity.x = FlxG.random.float(-10, 10);
     
-        line.scale.set(missNumScale, missNumScale);
+        line.scale.set(.5, .5);
         line.updateHitbox();
     
         line.antialiasing = Options.antialiasing;
@@ -246,7 +165,7 @@ function createRatingMiss() {
     rating.velocity.y -= FlxG.random.int(140, 175);
     rating.velocity.x -= FlxG.random.int(0, 10);
 
-    rating.scale.set(missRatingScale, missRatingScale);
+    rating.scale.set(.7, .7);
     rating.updateHitbox();
 
     rating.antialiasing = Options.antialiasing;
