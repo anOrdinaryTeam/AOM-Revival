3e87c7da829e4fd24db8680e979b4495ab543d86
Solex <95106797+SantiYea@users.noreply.github.com>
2026-09-18 02:57:27 -0300

    m,dkm,d,d

--- data/notes/FNM/Bullet Note.hx
+++ data/notes/FNM/Bullet Note.hx
@@ -1,3 +1,5 @@
+using StringTools;
+
 var redSplash:FlxSprite = new FlxSprite().makeSolid(FlxG.width, FlxG.height, FlxColor.RED);
 
 function create() {
@@ -6,18 +8,25 @@ function create() {
     add(redSplash);
 }
 
-function DontRepeatCode(dir:Int, isMiss:Bool) {
+function DontRepeatCode(dir:Int, isMiss:Bool, char:Character) {
     var jebusTrail:Character = strumLines.members[0].characters[1];
     jebusTrail.playSingAnim(dir, '-shoot');
     dad.playSingAnim(dir, '-shoot');
 
-    if (!isMiss) {
+    if (!isMiss) dodgeChecker(char, char.curCharacter);
+    camGame.shake(0.01, 0.2);
+}
+
+function dodgeChecker(char:Character, name:String) {
+    if (name.contains('hank')) {
+        FlxTween.cancelTweensOf(char);
+        FlxTween.color(char, 0.2, FlxColor.CYAN, -1, {ease: FlxEase.quadInOut});
+    }
+    else {
         var playerTrail:Character = strumLines.members[1].characters[1];
         playerTrail.playAnim('dodge', true);
-        boyfriend.playAnim('dodge', true);
+        char.playAnim('dodge', true);
     }
-
-    camGame.shake(0.01, 0.2);
 }
 
 function onNoteCreation(e) {
@@ -28,15 +37,22 @@ function onNoteCreation(e) {
 function onPlayerHit(e) {
     if (e.noteType != 'FNM/Bullet Note') return;
     e.animCancelled = true;
-    DontRepeatCode(e.direction, false);
+    DontRepeatCode(e.direction, false, e.character);
 }
 
 function onPlayerMiss(e) {
     if (e.noteType != 'FNM/Bullet Note') return;
-    DontRepeatCode(e.direction, true);
+    DontRepeatCode(e.direction, true, e.character);
     e.healthGain -= 0.2;
     e.animCancelled = true;
-    boyfriend.playAnim('hit', true);
+
+    var isBf:Bool = e.character.curCharacter.contains('bf');
+    if (isBf)
+        boyfriend.playAnim('hit', true);
+    else {
+        FlxTween.cancelTweensOf(e.character);
+        FlxTween.color(e.character, 0.2, FlxColor.RED, -1, {ease: FlxEase.quadInOut});
+    }
 
     redSplash.alpha = 0.2;
     FlxTween.tween(redSplash, {alpha: 0}, 0.4, {startDelay: 0.125, ease: FlxEase.quadInOut});
